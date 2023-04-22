47c8e2530b51dab87b5ca8b9cda9f484b1f3f199
Marc Ochsner <marc.ochsner@woundzoom.com>
2023-04-22 15:06:06 -0400

    add actions

---
+++ src/app/shared/components/buttons/login-button.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '@auth0/auth0-angular';
+
+@Component({
+  selector: 'app-login-button',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <button class="button__login" (click)="handleLogin()">Log In</button>
+  `
+})
+export class LoginButtonComponent {
+
+  constructor(private auth: AuthService) {}
+
+  handleLogin(): void {
+    /* Add User Login to Angular */
+    this.auth.loginWithRedirect({
+      appState: {
+        target: '/profile', /* If you don't specify this appState.returnTo option, your users will be redirected by default to the / path after they log in. */
+        returnTo: '/profile' // Note, may be the same as "target"
+      },
+      /* Add User Sign-Up to Angular */
+      authorizationParams : {
+        screen_hint: "signup"
+      }
+    });
+  }
+}
---
+++ src/app/shared/components/buttons/signup-button.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '@auth0/auth0-angular';
+
+@Component({
+  selector: 'app-signup-button',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+  <button class="button__sign-up" (click)="handleSignUp()">Sign Up</button>
+  `
+})
+export class SignupButtonComponent {
+  constructor(private auth: AuthService) {}
+
+  handleSignUp(): void {
+    this.auth.loginWithRedirect({
+      appState: {
+        target: "/profile",
+      },
+      authorizationParams: {
+        screen_hint: 'signup',
+      },
+    });
+  }
+}
