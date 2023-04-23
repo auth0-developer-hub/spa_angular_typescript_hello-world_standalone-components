52c0a6317a6552afcdf2b22cf4322113221980c3
mochsner <marcaochsner@gmail.com>
2023-04-22 21:16:42 -0400

    Render tabs conditionally

--- src/app/shared/components/navigation/desktop/nav-bar-tabs.component.ts
+++ src/app/shared/components/navigation/desktop/nav-bar-tabs.component.ts
@@ -1,10 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 import { NavBarTabComponent } from './nav-bar-tab.component';
 
 @Component({
   standalone: true,
-  imports: [NavBarTabComponent],
+  imports: [CommonModule, NavBarTabComponent],
   selector: 'app-nav-bar-tabs',
   templateUrl: './nav-bar-tabs.component.html',
 })
-export class NavBarTabsComponent {}
+export class NavBarTabsComponent {
+  isAuthenticated$ = this.authService.isAuthenticated$;
+  constructor(private authService: AuthService) {}
+}
