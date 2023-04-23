1ad10f14b50513ec3ccfc7dd9ec5e86497dd9b7e
mochsner <marcaochsner@gmail.com>
2023-04-23 13:39:43 -0400

    Get User Profile Information in Angular

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,14 +1,37 @@
 import { Route } from '@angular/router';
+import { AuthGuard } from '@auth0/auth0-angular';
 
 export const ROUTES: Route[] = [
-  {path: '', pathMatch: 'full', loadComponent: () => import('./features/home/home.component').then(mod => mod.HomeComponent)},
-  {path: 'profile', loadComponent: () => import('./features/profile/profile.component').then(mod => mod.ProfileComponent)},
-  {path: 'public', loadComponent: () => import('./features/public/public.component').then(mod => mod.PublicComponent)},
-  {path: 'protected', loadComponent: () => import('./features/protected/protected.component').then(mod => mod.ProtectedComponent)},
-  {path: 'admin', loadComponent: () => import('./features/admin/admin.component').then(mod => mod.AdminComponent)},
+  {
+    path: '', 
+    pathMatch: 'full', 
+    loadComponent: () => import('./features/home/home.component').then(mod => mod.HomeComponent),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'profile', 
+    loadComponent: () => import('./features/profile/profile.component').then(mod => mod.ProfileComponent)
+  },
+  {
+    path: 'public', 
+    loadComponent: () => import('./features/public/public.component').then(mod => mod.PublicComponent)
+  },
+  {
+    path: 'protected', 
+    loadComponent: () => import('./features/protected/protected.component').then(mod => mod.ProtectedComponent),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'admin', 
+    loadComponent: () => import('./features/admin/admin.component').then(mod => mod.AdminComponent),
+    canActivate: [AuthGuard]
+  },
   {
     path: 'callback',
     loadComponent: () => import('./features/callback/callback.component').then(mod => mod.CallbackComponent)
   },
-  {path: '**', loadComponent: () => import('./features/not-found/not-found.component').then(mod => mod.NotFoundComponent)},
+  {
+    path: '**', 
+    loadComponent: () => import('./features/not-found/not-found.component').then(mod => mod.NotFoundComponent)
+  },
 ];
